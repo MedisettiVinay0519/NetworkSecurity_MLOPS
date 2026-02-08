4c436bb732b7ce0f5f29ee01aaa2829e3fd4285a
MedisettiVinay0519 <vinaymedisetti19@gmail.com>
2026-02-08 11:23:48 +0530

    Updated

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -24,12 +24,3 @@ class NetworkSecurityException(Exception):
             "error message [{2}]"
         ).format(self.file_name, self.lineno, str(self.error_message))
 
-
-if __name__ == "__main__":
-    try:
-        logger.logging.info("Enter the try block")
-        a = 1 / 0
-        print("This will not be printed", a)
-
-    except Exception as e:
-        raise NetworkSecurityException(e, sys)
